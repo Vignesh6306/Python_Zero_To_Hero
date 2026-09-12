8e0175231738feb41ab678450830b4eca68013c8
Vignesh6306 <vigneshwaran0636@gmail.com>
2026-09-12 18:49:51 +0530

    How to get inputs from the user

---
+++ Beginner Module/Inputs_From_User.py
@@ -0,0 +1,12 @@
+Name = input("Enter Your Name : ")
+
+Age = int(input("Enter Your age : "))
+
+Weight = float(input("Enter Your weight : "))
+
+Marks = list(map(int, input("Enter your 5 subjects marks : ").split()))
+
+print(Name)
+print(Age)
+print(Weight)
+print(Marks)
