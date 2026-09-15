4b16487eac879590846db61a7789f811e9c9fb81
Vignesh6306 <vigneshwaran0636@gmail.com>
2026-09-15 19:53:06 +0530

    Operations & Methods Using Strings

---
+++ Intermediate Module/Strings_2.py
@@ -0,0 +1,21 @@
+A = "meThoDs oF sTriNgS"
+# Capitalize : Converts the first letter into capital
+print(A.capitalize())
+
+# Casefold : Similar to lower case. But more aggressive
+print(A.casefold())
+
+# Title : Converts the First letter in the sequence as Capital
+print(A.title())
+
+# Lower : Coverts everything into Lower-case
+print(A.lower())
+
+# Lower : Coverts everything into Upper-case
+print(A.upper())
+
+# SwapCase : Coverts Lower into Upper And Upper into Lower vice versa
+print(A.swapcase())
+
+
+
